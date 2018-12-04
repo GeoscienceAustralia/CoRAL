728f7e31dfa3c8cc62e5738be8dfbfd2fd5f0b4e
Matt Garthwaite <Matt.Garthwaite@ga.gov.au>
2018-12-04 14:11:21 +1100

    split plotting functions out of script in to separate module

--- Calc_CR_response.py
+++ Calc_CR_response.py
@@ -1,12 +1,13 @@
-import matplotlib
-matplotlib.use('Agg')
-from matplotlib import pyplot as plt
-from matplotlib.patches import RegularPolygon
-from matplotlib.colors import LogNorm
+"""
+Ths is an example python script for running CoRAL analysis
+
+Example usage: python3 Calc_CR_response.py
+"""
 import glob, re
 import numpy as np
 from datetime import datetime
 from coral.corner_reflector import *
+from coral.plot import *
 
 # start/end date for plots
 start = datetime(2018, 7, 1)
@@ -78,82 +79,12 @@ for name, cr in sites.items():
 
     cr_pos = np.array([sub_im, sub_im])
 
-    # set black/white colormap for plots
-    cmap = plt.set_cmap('gist_gray')
-
-    # draw new plot
-    #fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-    fig, ax1 = plt.subplots(1, 1, sharey=True)
-    #ax = fig.add_subplot(1,1,1)
-    cax = ax1.matshow(avgI_d, vmin=-20, vmax=10, cmap=cmap)
-    #cax = ax2.matshow(avgI_d, vmin=-20, vmax=10, cmap=cmap)
-
-    # define target window
-    p1 = RegularPolygon(cr_pos, 4, (targ_win_sz/2)+1, \
-                            orientation = np.pi / 4, linewidth=1, \
-                            edgecolor='r',facecolor='none')
-    # define clutter window
-    p2 = RegularPolygon(cr_pos, 4, (clt_win_sz/2)+2, \
-                            orientation = np.pi / 4, linewidth=1, \
-                            edgecolor='y',facecolor='none')
-
-    # add windows to plot
-    ax1.add_patch(p1)
-    ax1.add_patch(p2)
-
-    # add text labels
-    ax1.text(45, 42, name, color='w', fontsize=10)
-
-    # plot labels
-    ax1.set_xlabel('Range')
-    ax1.set_ylabel('Azimuth')
-
-    # add colorbar
-    cbar = fig.colorbar(cax)
-    cbar.set_label('dB')
-
-    # add title
-    ax1.set_title('Mean intensity at %s' % name)
-
-    # x-axis labels at bottom
-    ax1.xaxis.set_tick_params(labeltop='off', labelbottom='on')
-
-    # fit subplots and save fig
-    fig.tight_layout()
-    #fig.set_size_inches(w=6,h=4)
-
-    # save PNG file
-    fig.savefig('mean_intensity_%s.png' % name, dpi=300, bbox_inches='tight')
+    # Plot mean intensity image
+    plot_mean_intensity(avgI_d, cr_pos, targ_win_sz, clt_win_sz, name)
 
     # Plot RCS_SCR time series
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    plt.plot(t_d, rcs_d, 'ro-', label='RCS')
-    plt.plot(t_d, scr_d, 'bo-', label='SCR')
-    plt.xlim(start, end)
-    plt.ylim(0, 40)
-    plt.xlabel('Date')
-    plt.ylabel('RCS / SCR (dB)')
-    plt.legend(loc=4)
-    plt.grid(True)
-    plt.title('Corner Reflector response at %s' % name)
-    for label in ax.get_xticklabels():
-        label.set_rotation(90)
-    fig.savefig('rcs_scr_%s.png' % name, dpi=300, bbox_inches='tight')
-
+    plot_rcs_scr(t_d, rcs_d, scr_d, start, end, name)
 
     # Plot average clutter time series
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    plt.plot(t_d, Avg_clt_d, 'bo-', label='Clutter')
-    plt.xlim(start, end)
-    plt.ylim(-16, -2)
-    plt.xlabel('Date')
-    plt.ylabel('Average Clutter (dB)')
-    plt.legend(loc=1)
-    plt.grid(True)
-    plt.title('Average Clutter at %s' % name)
-    for label in ax.get_xticklabels():
-        label.set_rotation(90)
-    fig.savefig('clutter_%s.png' % name, dpi=300, bbox_inches='tight')
+    plot_clutter(t_d, Avg_clt_d, start, end, name)
 
---
+++ coral/plot.py
@@ -0,0 +1,99 @@
+"""
+This python module contains functions for plotting CoRAL output
+"""
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+from matplotlib.patches import RegularPolygon
+from matplotlib.colors import LogNorm
+import numpy as np
+
+
+def plot_mean_intensity(avgI, cr_pos, targ_win_sz, clt_win_sz, name):
+    '''Plot image of mean SAR intensity'''
+    # set black/white colormap for plots
+    cmap = plt.set_cmap('gist_gray')
+
+    # draw new plot
+    #fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+    fig, ax1 = plt.subplots(1, 1, sharey=True)
+    #ax = fig.add_subplot(1,1,1)
+    cax = ax1.matshow(avgI, vmin=-20, vmax=10, cmap=cmap)
+    #cax = ax2.matshow(avgI_d, vmin=-20, vmax=10, cmap=cmap)
+
+    # define target window
+    p1 = RegularPolygon(cr_pos, 4, (targ_win_sz/2)+1, \
+                            orientation = np.pi / 4, linewidth=1, \
+                            edgecolor='r',facecolor='none')
+    # define clutter window
+    p2 = RegularPolygon(cr_pos, 4, (clt_win_sz/2)+2, \
+                            orientation = np.pi / 4, linewidth=1, \
+                            edgecolor='y',facecolor='none')
+
+    # add windows to plot
+    ax1.add_patch(p1)
+    ax1.add_patch(p2)
+
+    # add text labels
+    #ax1.text(45, 42, name, color='w', fontsize=10)
+
+    # plot labels
+    ax1.set_xlabel('Range')
+    ax1.set_ylabel('Azimuth')
+
+    # add colorbar
+    cbar = fig.colorbar(cax)
+    cbar.set_label('dB')
+
+    # add title
+    ax1.set_title('Mean intensity at %s' % name)
+
+    # x-axis labels at bottom
+    ax1.xaxis.set_tick_params(labeltop='off', labelbottom='on')
+
+    # fit subplots and save fig
+    fig.tight_layout()
+    #fig.set_size_inches(w=6,h=4)
+
+    # save PNG file
+    fig.savefig('mean_intensity_%s.png' % name, dpi=300, bbox_inches='tight')
+
+    return
+
+
+def plot_rcs_scr(t, rcs, scr, start, end, name):
+    '''Plot RCS and SCR time series'''
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    plt.plot(t, rcs, 'ro-', label='RCS')
+    plt.plot(t, scr, 'bo-', label='SCR')
+    plt.xlim(start, end)
+    plt.ylim(0, 40)
+    plt.xlabel('Date')
+    plt.ylabel('RCS / SCR (dB)')
+    plt.legend(loc=4)
+    plt.grid(True)
+    plt.title('Corner Reflector response at %s' % name)
+    for label in ax.get_xticklabels():
+        label.set_rotation(90)
+    fig.savefig('rcs_scr_%s.png' % name, dpi=300, bbox_inches='tight')
+
+    return
+
+def plot_clutter(t, clt, start, end, name):
+    '''Plot average clutter time series'''
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    plt.plot(t, clt, 'bo-', label='Clutter')
+    plt.xlim(start, end)
+    plt.ylim(-16, -2)
+    plt.xlabel('Date')
+    plt.ylabel('Average Clutter (dB)')
+    plt.legend(loc=1)
+    plt.grid(True)
+    plt.title('Average Clutter at %s' % name)
+    for label in ax.get_xticklabels():
+        label.set_rotation(90)
+    fig.savefig('clutter_%s.png' % name, dpi=300, bbox_inches='tight')
+
+    return
