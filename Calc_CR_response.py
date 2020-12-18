d592841b6e00e0527af23fea12530696d9f7e3b4
Thomas Fuhrmann <Thomas.Fuhrmann@ga.gov.au>
2020-12-18 12:08:41 +1100

    added usage of a config file containing paths to input files and parameters used for the target response analysis, added required input files to 'data' dir

--- Calc_CR_response.py
+++ Calc_CR_response.py
@@ -1,53 +1,277 @@
 """
-Ths is an example python script for running CoRAL analysis
+Ths is the main python script for running CoRAL analysis
 
-Example command line usage: python3 Calc_CR_response.py
+Example command line usage: python3 Calc_CR_response_config.py /path/to/config_file
+An example for the config_file is here: ./data/coral_serf.conf
+An example for the input file list is here: ./data/mli_desc.list
+An example for the file containing target coordinates is here: ./data/site_lat_lon_hgt_date1_date2_az_rg_initial.txt
+
+required packages to be installed (on Gadi):
+# pip install --user joblib
+# pip install --user rasterio
 """
-import glob
 import numpy as np
 from datetime import datetime
 from coral.corner_reflector import loop
+from coral.dataio import read_radar_coords, write_radar_coords
 from coral.plot import *
-
-# start/end date for plots
-start = datetime(2018, 7, 1)
-end = datetime(2018, 11, 1)
-
-# Get list of image files in current directory
-files = []
-for file in glob.glob("data/*.tif"):
-    files.append(file)
-
-files.sort()
-
-# final target positions manual entry
-sites = {
-#   site name            rg   az
-    'SERF' : np.array([[ 87, 110]]),
-}
-
-#cropped image size
-sub_im = 51
-
-# define target_window size
-targ_win_sz = 5
-
-# define clutter window size
-clt_win_sz = 9
-
-# loop through all corner reflector sites
-for name, cr in sites.items():
-    print(name,'is desc',cr[0])
-
-    avgI, rcs, scr, Avg_clt, t, cr_new, cr_pos = loop(files, sub_im, cr[0], targ_win_sz, clt_win_sz)
-
-    # Plot mean intensity image
-    plot_mean_intensity(avgI, cr_pos, targ_win_sz, clt_win_sz, name)
-
-    # Plot RCS_SCR time series
-    plot_rcs_scr(t, rcs, scr, start, end, name)
-
-    # Plot average clutter time series
-    plot_clutter(t, Avg_clt, start, end, name, "Descending")
+from coral.plot2 import *
+from joblib import Parallel, delayed
+import multiprocessing
+import sys, os, os.path
+from coral import config as cf
+from configparser import ConfigParser
 
 
+class Configuration:
+    def __init__(self, config_file_path):
+        parser = ConfigParser()
+        parser.optionxform = str
+        # mimic header to fulfil the requirement for configparser
+        with open(config_file_path) as stream:
+            parser.read_string("[root]\n" + stream.read())
+
+        for key, value in parser["root"].items():
+            self.__dict__[key] = value
+
+def _params_from_conf(config_file):
+    config_file = os.path.abspath(config_file)
+    config = Configuration(config_file)
+    params = config.__dict__
+    return params
+
+
+print('')
+if len(sys.argv) != 2:
+    print('Exiting: Provide path to config-file as command line argument')
+    print('')
+    print('Usage: python3 Calc_CR_response.py <config-file>')
+    exit()
+else:
+    config_file = sys.argv[1]
+    print(f'Looking for CoRAL input data defined in {config_file}')
+
+
+params = _params_from_conf(config_file)
+
+print(f'Results will be saved into {params[cf.OUT_DIR]}')
+# Start the processing
+print(' ')
+print('Running CoRAL...')
+print(' ')
+# used to calculate runtime
+run_start = datetime.now()
+
+
+# if no datapath is given in the config file, this geometry is not used
+if params[cf.ASC_LIST]:
+    print(f'Reading data from file list {params[cf.ASC_LIST]}')
+    # read the paths to ascending-pass backscatter images
+    with open(params[cf.ASC_LIST]) as f_in:
+        files_a = [line.rstrip() for line in f_in]
+        files_a.sort()
+    # read the ascending-pass radar coordinates of targets
+    filename = params[cf.ASC_CR_FILE_ORIG]
+    sites_a, az_a, rg_a = read_radar_coords(filename)
+else:
+    print('No ascending-pass file list supplied')
+    # read asc pass data files
+if params[cf.DESC_LIST]:
+    print(f'Reading data from file list {params[cf.DESC_LIST]}')
+    # read the paths to descending-pass backscatter images
+    with open(params[cf.DESC_LIST]) as f_in:
+        files_d = [line.rstrip() for line in f_in]
+        files_d.sort()
+    # read the descending-pass radar coordinates of targets
+    filename = params[cf.DESC_CR_FILE_ORIG]
+    sites_d, az_d, rg_d = read_radar_coords(filename)
+else:
+    print('No descending-pass file list supplied')
+
+
+# check if CR files have the same length
+if params[cf.ASC_LIST] and params[cf.DESC_LIST]:
+    if sites_a == sites_d:
+        print("CR files for asc and desc tracks contain same set of sites.")
+        keys = sites_a
+        values = (np.array([[rg_a[i], az_a[i]],[rg_d[i], az_d[i]]], dtype=int) \
+                 for i in range(0, len(sites_a)))
+    else:
+        print("ERROR: different CR sites given for asc and desc track.")
+        sys.exit()
+    print(' ')    
+elif params[cf.ASC_LIST] and not params[cf.DESC_LIST]:
+    keys = sites_a
+    values = (np.array([[rg_a[i], az_a[i]],[-999, -999]], dtype=int) \
+             for i in range(0, len(sites_a)))
+elif params[cf.DESC_LIST] and not params[cf.ASC_LIST]:
+    keys = sites_d
+    values = (np.array([[-999, -999],[rg_d[i], az_d[i]]], dtype=int) \
+             for i in range(0, len(sites_d)))
+
+sites = dict(zip(keys, values))
+
+
+##################################
+# function for parallel loop processing
+names = sites.keys()
+# note that dictionaries are unsorted and the variable name is hence not ordered
+def processInput(name):
+
+    cr = sites[name]
+
+    if params[cf.ASC_LIST]:
+        avgI_a, rcs_a, scr_a, clt_a, t_a, cr_new_a, cr_pos_a = loop(files_a, int(params[cf.SUB_IM]), cr[0], int(params[cf.TARG_WIN_SZ]), int(params[cf.CLT_WIN_SZ]))
+    if params[cf.DESC_LIST]:
+        avgI_d, rcs_d, scr_d, clt_d, t_d, cr_new_d, cr_pos_d = loop(files_d, int(params[cf.SUB_IM]), cr[1], int(params[cf.TARG_WIN_SZ]), int(params[cf.CLT_WIN_SZ]))
+
+    if params[cf.ASC_LIST] and params[cf.DESC_LIST]:
+        return name, cr_pos_a, avgI_a, rcs_a, scr_a, clt_a, t_a, cr_new_a, \
+               cr_pos_d, avgI_d, rcs_d, scr_d, clt_d, t_d, cr_new_d
+    elif params[cf.ASC_LIST] and not params[cf.DESC_LIST]:
+        return name, cr_pos_a, avgI_a, rcs_a, scr_a, clt_a, t_a, cr_new_a
+    elif params[cf.DESC_LIST] and not params[cf.ASC_LIST]:
+        return name, cr_pos_d, avgI_d, rcs_d, scr_d, clt_d, t_d, cr_new_d
+
+
+# parallel processing of MLI read and RCS, SCR calculation
+num_cores = multiprocessing.cpu_count()
+# num_cores results in 32 on the NCI which in turn results in an error
+# hence the number of 16 cores is hard-coded here
+results = Parallel(n_jobs=16)(delayed(processInput)(name) for name in names)
+
+
+# extract results and plot images
+
+# create output dir if it doesn't exist
+if not os.path.exists(params[cf.OUT_DIR]):
+    os.makedirs(params[cf.OUT_DIR])
+
+print(' ') 
+print('Creating output data...')
+print(' ') 
+for i in range(0, len(names)):
+    
+    # ascending and descending CRs in one plot
+    if params[cf.ASC_LIST] and params[cf.DESC_LIST]:
+        # read result arrays of parallel function, both geometries
+        name = results[i][0]
+        cr_pos_a = results[i][1]
+        avgI_a = results[i][2]
+        rcs_a = results[i][3]
+        scr_a = results[i][4]
+        clt_a = results[i][5]
+        t_a = results[i][6]
+        cr_new_a = results[i][7]
+        cr_pos_d = results[i][8]
+        avgI_d = results[i][9]
+        rcs_d = results[i][10]
+        scr_d = results[i][11]
+        clt_d = results[i][12]
+        t_d = results[i][13]
+        cr_new_d = results[i][14]
+        
+        # add new coords to sites dictionary
+        cr = np.array([cr_new_a, cr_new_d])
+        sites[name] = cr
+        
+        # Visualisation
+        print('Site %s' % name)
+        # Plot mean intensity image
+        plot_mean_intensity2(avgI_a, avgI_d, cr_pos_a, cr_pos_d, int(params[cf.TARG_WIN_SZ]), int(params[cf.CLT_WIN_SZ]), name, params[cf.OUT_DIR])
+         
+        # extract start and end date
+        start_time = min(t_a[0], t_d[0])
+        end_time = max(t_a[-1], t_d[-1])
+        margin = (end_time - start_time)/50
+        start = start_time - margin
+        end = end_time + margin
+        
+        # Plot average clutter time series
+        plot_clutter2(t_a, t_d, clt_a, clt_d, start, end, name, params[cf.OUT_DIR])
+        # Plot scr time series
+        plot_scr2(t_a, t_d, scr_a, scr_d, start, end, name, params[cf.OUT_DIR])
+        # Plot rcs time series
+        plot_rcs2(t_a, t_d, rcs_a, rcs_d, start, end, name, params[cf.OUT_DIR])
+        
+        # write RCS data to file
+        filename_out = params[cf.OUT_DIR] + "/rcs_values_" + name + "_" + "Ascending.txt"
+        fout = open(filename_out,'w')
+        for time, value in zip(t_a,rcs_a):
+            timestr = time.strftime("%Y%m%d")
+            fout.write("%s %f\n" % (timestr, value))
+        fout.close()
+        filename_out = params[cf.OUT_DIR] + "/rcs_values_" + name + "_" + "Descending.txt"
+        fout = open(filename_out,'w')
+        for time, value in zip(t_d,rcs_d):
+            timestr = time.strftime("%Y%m%d")
+            fout.write("%s %f\n" % (timestr, value))
+        fout.close() 
+         
+    # one geometry (ascending or descending)    
+    else:
+        # read result arrays of parallel function, one geometry
+        name = results[i][0]
+        cr_pos = results[i][1]
+        avgI = results[i][2]
+        rcs = results[i][3]
+        scr = results[i][4]
+        clt = results[i][5]
+        t = results[i][6]
+        cr_new = results[i][7]
+
+        # add new coords to sites dictionary
+        if params[cf.ASC_LIST]:
+           geom='Ascending'
+           cr = np.array([cr_new, [-999, -999]])
+        if params[cf.DESC_LIST]:
+           geom='Descending'
+           cr = np.array([[-999, -999], cr_new])
+        sites[name] = cr
+
+
+        # Visualisation
+        print('Site %s' % name)
+        # Plot mean intensity image
+        plot_mean_intensity(avgI, cr_pos, int(params[cf.TARG_WIN_SZ]), int(params[cf.CLT_WIN_SZ]), name, params[cf.OUT_DIR])
+        
+        # extract start and end date
+        start_time = t[0]
+        end_time = t[-1]
+        margin = (end_time - start_time)/50
+        start = start_time - margin
+        end = end_time + margin
+        
+        # Plot average clutter time series
+        plot_clutter(t, clt, start, end, name, geom, params[cf.OUT_DIR])
+        # Plot scr time series
+        plot_scr(t, scr, start, end, name, geom, params[cf.OUT_DIR])
+        # Plot rcs time series
+        plot_rcs(t, rcs, start, end, name, geom, params[cf.OUT_DIR])
+        # Plot RCS_SCR time series
+        plot_rcs_scr(t, rcs, scr, start, end, name, params[cf.OUT_DIR])
+
+        
+        # write RCS data to file
+        filename_out = params[cf.OUT_DIR] + "/rcs_values_" + name + "_" + geom + ".txt"
+        fout = open(filename_out,'w')
+        for time, value in zip(t,rcs):
+            timestr = time.strftime("%Y%m%d")
+            fout.write("%s %f\n" % (timestr, value))
+        fout.close()  
+
+
+# write updated radar coordinates to a new file
+print(' ')
+if params[cf.ASC_LIST]:
+    # new file with updated radar coordinates of CRs
+    write_radar_coords(params[cf.ASC_CR_FILE_ORIG], params[cf.ASC_CR_FILE_NEW], sites, "asc")
+if params[cf.DESC_LIST]:
+    # new file with updated radar coordinates of CRs
+    write_radar_coords(params[cf.DESC_CR_FILE_ORIG], params[cf.DESC_CR_FILE_NEW], sites, "desc")
+        
+
+# print out runtime
+runtime = datetime.now() - run_start
+print(' ')
+print('Runtime: %s' % runtime)
---
+++ coral/config.py
@@ -0,0 +1,142 @@
+"""
+This Python module contains utilities to parse CoRAL configuration
+files.
+"""
+from typing import Dict
+import os
+
+
+# constants for lookups
+#: STR; Name of input interferogram list file
+ASC_LIST = 'backscatter_data_asc'
+DESC_LIST = 'backscatter_data_desc'
+ASC_CR_FILE_ORIG = 'cr_file_asc'
+ASC_CR_FILE_NEW = 'cr_file_asc_new'
+DESC_CR_FILE_ORIG = 'cr_file_desc'
+DESC_CR_FILE_NEW = 'cr_file_desc_new'
+OUT_DIR = 'path_out'
+TARG_WIN_SZ = 'target_window_size'
+CLT_WIN_SZ = 'clutter_window_size'
+SUB_IM = 'sub_image_size'
+
+
+# Lookup to help convert args to correct type/defaults
+# format is	key : (conversion, default value)
+# None = no conversion
+PARAM_CONVERSION = {
+    TARG_WIN_SZ: (int, 3),
+    CLT_WIN_SZ: (int, 7),
+    SUB_IM: (int, 51),
+}
+
+PATHS = [
+    ASC_LIST,
+    DESC_LIST,
+    ASC_CR_FILE_ORIG,
+    ASC_CR_FILE_NEW,
+    DESC_CR_FILE_ORIG,
+    DESC_CR_FILE_NEW,
+    OUT_DIR,
+]
+
+
+def get_config_params(path: str) -> Dict:
+    """
+    Reads the parameters file provided by the user and converts it into
+    a dictionary.
+
+    Args:
+        path: Absolute path to the parameters file.
+        validate: Validate the parameters if True, otherwise skip validation.
+        step: The current step of the PyRate workflow.
+
+    Returns:
+       A dictionary of parameters.
+    """
+    txt = ''
+    with open(path, 'r') as inputFile:
+        for line in inputFile:
+            if any(x in line for x in PATHS):
+                pos = line.find('~')
+                if pos != -1:
+                    # create expanded line
+                    line = line[:pos] + os.environ['HOME'] + line[(pos + 1):]
+            txt += line
+    params = _parse_conf_file(txt)
+
+    return params
+
+
+def _parse_conf_file(content) -> Dict:
+    """
+    Converts the parameters from their text form into a dictionary.
+
+    Args:
+        content: Parameters as text.
+
+    Returns:
+        A dictionary of parameters.
+    """
+
+    def _is_valid(line):
+        """
+        Check if line is not empty or has % or #
+        """
+        return line != "" and line[0] not in "%#"
+
+    lines = [ln.split() for ln in content.split('\n') if _is_valid(ln)]
+
+    # convert "field:   value" lines to [field, value]
+    kvpair = [(e[0].rstrip(":"), e[1]) for e in lines if len(e) == 2] + \
+             [(e[0].rstrip(":"), None) for e in lines if len(e) == 1]
+    parameters = dict(kvpair)
+    for p in PATHS:
+        if p not in parameters:
+            parameters[p] = None
+
+    for p in INT_KEYS:
+        if p not in parameters:
+            parameters[p] = '0'  # insert dummies
+
+    parameters = _handle_extra_parameters(parameters)
+
+    if not parameters:
+        raise ConfigException('Cannot parse any parameters from config file')
+
+    return _parse_pars(parameters)
+
+
+# todo: check why conversion of parameters to int is not working properly
+def _parse_pars(pars) -> Dict:
+    """
+    Takes dictionary of parameters, converting values to required type
+    and providing defaults for missing values.
+
+    Args:
+        pars: Parameters dictionary.
+
+    Returns:
+        Dictionary of converted (and optionally validated) parameters.
+    """
+    # Fallback to default for missing values and perform conversion.
+    for k in PARAM_CONVERSION:
+        if pars.get(k) is None:
+            pars[k] = PARAM_CONVERSION[k][1]
+            # _logger.warning(f"No value found for parameter '{k}'. Using "f"default value {pars[k]}.")
+        else:
+            conversion_func = PARAM_CONVERSION[k][0]
+            if conversion_func:
+                try:
+                    pars[k] = conversion_func(pars[k])
+                except ValueError as e:
+                    _logger.error(
+                        f"Unable to convert '{k}': {pars[k]} to " f"expected type {conversion_func.__name__}.")
+                    raise e
+
+    return pars
+
+
+class ConfigException(Exception):
+    """
+    Default exception class for configuration errors.
+    """
